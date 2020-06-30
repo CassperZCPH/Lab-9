650d57201f18b1c8f42ef1092da1f093611858ac
Leo <46826012+CassperZCPH@users.noreply.github.com>
2020-06-30 05:10:36 +0300

    Add files via upload

---
+++ Lab 9/Calculator.cs
@@ -0,0 +1,156 @@
+﻿using System;
+
+namespace lab9
+{
+    delegate void CalculatorUpdateOutput(Calculator sender, double value, int precision);
+    delegate void CalculatorInternalError(Calculator sender, string message);
+
+    enum CalculatorOperation { Add, Sub, Mul, Div, Sqrt }
+
+    class Calculator
+    {
+        double? input = null;
+        double? result = null;
+        bool point = false;
+        int? count = null;
+
+        CalculatorOperation? op = null;
+
+        public event CalculatorUpdateOutput DidUpdateValue;
+        public event CalculatorInternalError InputError;
+        public event CalculatorInternalError EqullyError;
+        //public event CalculatorInternalError MinusSqrtError;
+
+        public void AddDigit(int digit)
+        {
+            if (input.HasValue && Math.Log10(input.Value) > 9)
+            {
+                InputError?.Invoke(this, "Value is too long for this calculator");
+                return;
+            }
+            if (count > 8)
+            {
+                InputError?.Invoke(this, "Value is too long for this calculator");
+                return;
+            }
+            if (!op.HasValue)
+            {
+                result = null;
+            }
+
+            if (point)
+            {
+                count = (count ?? 0);
+                input = ((input ?? 0) * Math.Pow(10, (double)count) + (double)digit / 10) / Math.Pow(10, (double)count);
+                DidUpdateValue?.Invoke(this, input.Value, 0);
+                count++;
+            }
+            else
+            {
+                input = (input ?? 0) * 10 + digit;
+                DidUpdateValue?.Invoke(this, input.Value, 0);
+            }
+        }
+
+        public void AddOperation(CalculatorOperation op)
+        {
+            if (input.HasValue && result.HasValue && this.op.HasValue)
+            {
+                Compute();
+            }
+
+            if (!input.HasValue)
+            {
+                result = 0;
+            }
+
+            if (!result.HasValue)
+            {
+                result = input;
+                input = null;
+            }
+            this.op = op;
+        }
+
+        public void Compute()
+        {
+            switch (op)
+            {
+                case CalculatorOperation.Add:
+                    result = result + (input ?? 0);
+                    DidUpdateValue?.Invoke(this, result.Value, 0);
+                    input = 0;
+                    break;
+
+                case CalculatorOperation.Div:
+                    if (input != null && input.Value != 0)
+                    {
+                        result = result / (input ?? 0);
+                        DidUpdateValue?.Invoke(this, result.Value, 0);
+                        input = 0;
+                        break;
+                    }
+                    else
+                    {
+                        EqullyError?.Invoke(this, "Division by zero");
+                        break;
+                    }
+
+                case CalculatorOperation.Mul:
+                    result = result - (input ?? 0);
+                    DidUpdateValue?.Invoke(this, result.Value, 0);
+                    input = 0;
+                    break;
+
+                case CalculatorOperation.Sub:
+                    result = result * (input ?? 0);
+                    DidUpdateValue?.Invoke(this, result.Value, 0);
+                    input = 0;
+                    break;
+
+                case CalculatorOperation.Sqrt:
+                    if (input < 0)
+                    {
+                        InputError?.Invoke(this, "Negative number under the root");
+                        return;
+                    }
+                    input = Math.Sqrt(input ?? 0) * 1.0;
+                    DidUpdateValue?.Invoke(this, input.Value, 0);
+                    break;
+            }
+            op = null;
+        }
+
+        public void Clear()
+        {
+            input = null;
+            DidUpdateValue?.Invoke(this, 0, 0);
+        }
+
+        public void ClearAll()
+        {
+            input = null;
+            result = null;
+            op = null;
+            DidUpdateValue?.Invoke(this, 0, 0);
+        }
+
+        public void Point()
+        {
+            point = true;
+        }
+
+        public void Polar()
+        {
+            input = -(input);
+            DidUpdateValue?.Invoke(this, input.Value, 0);
+        }
+        
+        public void CountPointOff()
+        {
+            count = null;
+            point = false;
+        }
+    }
+}
+
---
+++ Lab 9/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace lab9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculator calc;
+        public MainWindow()
+        {
+            InitializeComponent();
+            calc = new Calculator();
+            calc.DidUpdateValue += CalculatorDidUpdateValue;
+            calc.InputError += CalculatorInputError;
+            calc.EqullyError += CalculatorInternalError;
+            //calc.MinusSqrtError += CalculatorMinusError;
+            calc.Clear();
+        }
+        private void CalculatorMinusError(Calculator sender, string message)
+        {
+            MessageBox.Show(message, "Error");
+        }
+
+        private void CalculatorInputError(Calculator sender, string message)
+        {
+            MessageBox.Show(message, "Error");
+        }
+
+        private void CalculatorDidUpdateValue(Calculator sender, double value, int precision)
+        {
+            label1.Content = value.ToString();
+        }
+
+        private void CalculatorInternalError(Calculator sender, string message)
+        {
+            MessageBox.Show(message, "Error");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            if (int.TryParse(b.Content.ToString(), out int digit))
+            {
+                calc.AddDigit(digit);
+            }
+            else
+            {
+                switch (b.Name)
+                {
+                    case "minus":
+                        calc.CountPointOff();
+                        calc.AddOperation(CalculatorOperation.Mul);
+                        break;
+                    case "plus":
+                        calc.AddOperation(CalculatorOperation.Add);
+                        calc.CountPointOff();
+                        break;
+                    case "clear":
+                        calc.CountPointOff();
+                        calc.Clear();
+                        break;
+                    case "div":
+                        calc.AddOperation(CalculatorOperation.Div);
+                        calc.CountPointOff();
+                        break;
+                    case "mult":
+                        calc.AddOperation(CalculatorOperation.Sub);
+                        calc.CountPointOff();
+                        break;
+                    case "equally":
+                        calc.Compute();
+                        calc.CountPointOff();
+                        break;
+                    case "ClearAll":
+                        calc.ClearAll();
+                        calc.CountPointOff();
+                        break;
+                    case "point":
+                        label1.Content += ",";
+                        calc.Point();
+                        break;
+                    case "sqrt":
+                        calc.CountPointOff();
+                        calc.AddOperation(CalculatorOperation.Sqrt);
+                        break;
+                    case "polar":
+                        calc.CountPointOff();
+                        calc.Polar();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
